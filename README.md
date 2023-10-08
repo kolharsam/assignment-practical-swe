c2e8ccc789dca154fef15684295fcde2c3c5f21a
Sameer Kolhar <kolhar730@gmail.com>
2023-10-08 18:06:23 -0400

    add app

---
+++ README.md
@@ -0,0 +1,3 @@
+# Personal assignment: Deploy sample Django App to AWS EB
+
+- Link: http://env-ebdj.eba-mjpddyes.us-east-1.elasticbeanstalk.com/
---
+++ ebdj/ebdj/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+
+def health(request):
+    return HttpResponse('OK', status=200)
+
+def default(req):
+    return HttpResponse('Welcome, home!', status=200)
