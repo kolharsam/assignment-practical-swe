9c538bb78de5c6e18bce15c4bb5d8ae6228250d5
Sameer Kolhar <kolhar730@gmail.com>
2023-10-11 07:38:55 -0400

    update with all four tutorials work

--- ebdj/ebdj/settings.py
+++ ebdj/ebdj/settings.py
@@ -43,6 +43,16 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
